fc074f6d4b63873f03434d12cb7dd689271bb83c
SayuruDev <mailtosayuru@gmail.com>
2025-04-24 09:31:42 -0400

    84.Logout-Delete-Get All Users

--- backendsrc/users/user.route.js
+++ backendsrc/users/user.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("./user.model");
+const generateToken = require("../middleware/generateToken");
 const router = express.Router();
 
 // Register endpoint
@@ -27,11 +28,62 @@ router.post("/login", async (req, res) => {
     if (!isMatch) {
       return res.status(401).send({ message: "Password not match" });
     }
+    const token = await generateToken(user._id);
 
-    res.status(200).send({ message: "Logged in successfully", user });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
+    res.status(200).send({
+      message: "Logged in successfully",
+      token,
+      user: {
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+        role: user.role,
+        profileImage: user.profileImage,
+        bio: user.bio,
+        profession: user.profession,
+      },
+    });
   } catch (error) {
-    console.error("Error logged in user ", error);
+    console.error("Error logged in user", error);
     res.status(500).send({ message: "Error logged in user" });
+  }
+});
+
+// logout endpoint
+router.post("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.status(200).send({ message: "Logged out successfully" });
+});
+
+// delete a user
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    res.status(200).send({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting user", error);
+    res.status(500).send({ message: "Error deleting user" });
+  }
+});
+
+// get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({}, "id email role").sort({ createdAt: -1 });
+    res.status(200).send(users);
+  } catch (error) {
+    console.error("Error fetching users", error);
+    res.status(500).send({ message: "Error fetching user" });
   }
 });
 
