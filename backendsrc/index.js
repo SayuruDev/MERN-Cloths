15bce3a53eda365eb9a565bdfa720ed956228ebe
SayuruDev <mailtosayuru@gmail.com>
2025-04-24 07:21:48 -0400

    Create user - Password Hash

---
+++ backendsrc/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const boddyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ exteded: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+// all routes
+const authRoutes = require("./src/users/user.route");
+app.use("/api/auth", authRoutes);
+
+main()
+  .then(() => console.log("Mongodb is succesfully connected"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- backendsrc/users/user.model.js
+++ backendsrc/users/user.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userShecma = new Schema({
   username: { type: String, require: true, unique: true },
@@ -15,6 +16,15 @@ const userShecma = new Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// hashing passwords
+userShecma.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  user.password = hashedPassword;
+  next();
 });
 
 const User = new model("User", userShecma);
