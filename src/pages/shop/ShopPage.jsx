5f3013bd68ad2b312b87a9243655dc7d42dc0c0d
SayuruDev <mailtosayuru@gmail.com>
2025-04-25 21:39:59 -0400

    Product Page With Pagination -99

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -48,8 +48,18 @@ const ShopPage = () => {
     });
   };
 
+  // handle page change
+  const handlePageChange = (pageNumber) => {
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+    }
+  };
+
   if (isLoading) return <div>Loading....</div>;
   if (error) return <div>Error Loading Products...</div>;
+  const startProduct = (currentPage - 1) * ProductsPerPage + 1;
+  const endProduct = startProduct + products.length - 1;
+
   return (
     <>
       <section className="section__container bg-primary-light">
@@ -69,10 +79,44 @@ const ShopPage = () => {
           />
           <div>
             <h3 className="text-xl font-medium mb-4">
-              {" "}
-              Products Available : {products.length}
+              Showing {startProduct} to {endProduct} of {totalProducts} products
             </h3>
             <ProductCards products={products} />
+
+            {/* pagination controls */}
+            <div className="mt-6 flex justify-center">
+              <button
+                disabled={currentPage === 1}
+                onClick={() => handlePageChange(currentPage - 1)}
+                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md mr-2"
+              >
+                Previous
+              </button>
+
+              {[...Array(totalPages)].map((_, index) => (
+                <button
+                  key={index}
+                  onClick={() => handlePageChange(index + 1)}
+                  className={`px-4 py-2 ${
+                    currentPage === index + 1
+                      ? "bg-blue-500 text-white"
+                      : "bg-gray-300 text-gray-700"
+                  }
+                                        rounded-md mx-1
+                                        `}
+                >
+                  {index + 1}
+                </button>
+              ))}
+
+              <button
+                disabled={currentPage === totalPages}
+                onClick={() => handlePageChange(currentPage + 1)}
+                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md ml-2"
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </section>
